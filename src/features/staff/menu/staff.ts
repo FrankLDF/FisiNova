689fbce6cd8da1f67987554a3e0da4243dc8abdb
FrankLDF <frankluisduarte00@gmail.com>
2025-10-24 20:50:25 -0400

    agregando scroll por separado al navbar y a la pantalla de contenido

--- src/features/staff/menu/staff.ts
+++ src/features/staff/menu/staff.ts
@@ -11,7 +11,7 @@ import {
 
 export const staffMenu: AppMenuItem = {
   key: 'staff-management',
-  label: 'Gestión de Personal',
+  label: 'Personal',
   requiredRols: [Rol.ADMIN],
   children: [
     {
